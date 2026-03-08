a8645e5aab64038664ada4ac261cab2f93b48a44
Lucas Mazzetto <lucasrmazzetto@gmail.com>
2026-03-08 11:51:41 -0300

    feat: add generated feature map comparison outputs for labels 0-9

--- src/convnet.c
+++ src/convnet.c
@@ -1,45 +1,60 @@
 #include "convnet.h"
 #include "nn.h"
 
-void run_convnet(const int *input, unsigned int *class_indices)
+void convnet_forward(const int *input, int *conv1_out, int *pool1_out,
+                            int *conv2_out, int *pool2_out, int *linear1_out,
+                            int *linear2_out, int *output,
+                            unsigned int *class_indices)
 {
-    int out_conv1[BATCH_SIZE * CONV1_OUT_CHANNELS * CONV1_OUT_HEIGHT * CONV1_OUT_WIDTH];
-    int out_pool1[BATCH_SIZE * CONV1_OUT_CHANNELS * POOL1_OUT_HEIGHT * POOL1_OUT_WIDTH];
-    int out_conv2[BATCH_SIZE * CONV2_OUT_CHANNELS * CONV2_OUT_HEIGHT * CONV2_OUT_WIDTH];
-    int out_pool2[BATCH_SIZE * CONV2_OUT_CHANNELS * POOL2_OUT_HEIGHT * POOL2_OUT_WIDTH];
-    int out_linear1[BATCH_SIZE * LINEAR1_OUT_FEATURES];
-    int out_linear2[BATCH_SIZE * LINEAR2_OUT_FEATURES];
-    int output[BATCH_SIZE * OUTPUT_DIM];
-
-    conv2d_layer(input, layer_1_weight, out_conv1, layer_1_input_scale, layer_1_weight_scale_inv,
+    // conv1: input -> conv -> dequant -> ReLU
+    conv2d_layer(input, layer_1_weight, conv1_out, layer_1_input_scale, layer_1_weight_scale_inv,
                  layer_1_input_scale_inv, BATCH_SIZE, INPUT_CHANNELS, CONV1_OUT_CHANNELS,
                  INPUT_HEIGHT, INPUT_WIDTH, CONV1_OUT_HEIGHT, CONV1_OUT_WIDTH,
                  5, 5, 1, 1);
 
-    pooling2d(out_conv1, out_pool1, BATCH_SIZE, CONV1_OUT_CHANNELS, CONV1_OUT_HEIGHT,
+    // pool1: downsample conv1 features
+    pooling2d(conv1_out, pool1_out, BATCH_SIZE, CONV1_OUT_CHANNELS, CONV1_OUT_HEIGHT,
               CONV1_OUT_WIDTH, POOL1_OUT_HEIGHT, POOL1_OUT_WIDTH,
               2, 2, 2, 2);
 
-    conv2d_layer(out_pool1, layer_2_weight, out_conv2, layer_2_input_scale, layer_2_weight_scale_inv,
+    // conv2: pool1 -> conv -> dequant -> ReLU
+    conv2d_layer(pool1_out, layer_2_weight, conv2_out, layer_2_input_scale, layer_2_weight_scale_inv,
                  layer_2_input_scale_inv, BATCH_SIZE, CONV1_OUT_CHANNELS, CONV2_OUT_CHANNELS,
                  POOL1_OUT_HEIGHT, POOL1_OUT_WIDTH, CONV2_OUT_HEIGHT, CONV2_OUT_WIDTH,
                  5, 5, 1, 1);
 
-    pooling2d(out_conv2, out_pool2, BATCH_SIZE, CONV2_OUT_CHANNELS, CONV2_OUT_HEIGHT,
+    // pool2: downsample conv2 features
+    pooling2d(conv2_out, pool2_out, BATCH_SIZE, CONV2_OUT_CHANNELS, CONV2_OUT_HEIGHT,
               CONV2_OUT_WIDTH, POOL2_OUT_HEIGHT, POOL2_OUT_WIDTH,
               2, 2, 2, 2);
 
-    linear_layer(out_pool2, layer_3_weight, out_linear1, layer_3_input_scale, layer_3_weight_scale_inv,
+    // linear stack: pool2 -> fc1 -> fc2 -> fc3
+    linear_layer(pool2_out, layer_3_weight, linear1_out, layer_3_input_scale, layer_3_weight_scale_inv,
                  layer_3_input_scale_inv, BATCH_SIZE,
                  CONV2_OUT_CHANNELS * POOL2_OUT_HEIGHT * POOL2_OUT_WIDTH,
                  LINEAR1_OUT_FEATURES, 1);
 
-    linear_layer(out_linear1, layer_4_weight, out_linear2, layer_4_input_scale, layer_4_weight_scale_inv,
+    linear_layer(linear1_out, layer_4_weight, linear2_out, layer_4_input_scale, layer_4_weight_scale_inv,
                  layer_4_input_scale_inv, BATCH_SIZE, LINEAR1_OUT_FEATURES,
                  LINEAR2_OUT_FEATURES, 1);
 
-    linear_layer(out_linear2, layer_5_weight, output, layer_5_input_scale, layer_5_weight_scale_inv,
+    linear_layer(linear2_out, layer_5_weight, output, layer_5_input_scale, layer_5_weight_scale_inv,
                  layer_5_input_scale_inv, BATCH_SIZE, LINEAR2_OUT_FEATURES, OUTPUT_DIM, 0);
 
+    // final class index from logits
     argmax_over_cols(output, class_indices, BATCH_SIZE, OUTPUT_DIM);
+}
+
+void run_convnet(const int *input, unsigned int *class_indices)
+{
+    int conv1_out[BATCH_SIZE * CONV1_OUT_CHANNELS * CONV1_OUT_HEIGHT * CONV1_OUT_WIDTH];
+    int pool1_out[BATCH_SIZE * CONV1_OUT_CHANNELS * POOL1_OUT_HEIGHT * POOL1_OUT_WIDTH];
+    int conv2_out[BATCH_SIZE * CONV2_OUT_CHANNELS * CONV2_OUT_HEIGHT * CONV2_OUT_WIDTH];
+    int pool2_out[BATCH_SIZE * CONV2_OUT_CHANNELS * POOL2_OUT_HEIGHT * POOL2_OUT_WIDTH];
+    int linear1_out[BATCH_SIZE * LINEAR1_OUT_FEATURES];
+    int linear2_out[BATCH_SIZE * LINEAR2_OUT_FEATURES];
+    int output[BATCH_SIZE * OUTPUT_DIM];
+
+    convnet_forward(input, conv1_out, pool1_out, conv2_out, pool2_out,
+                           linear1_out, linear2_out, output, class_indices);
 }
